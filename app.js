922e46d97adb20023df9a98831975d9aab459acc
Clifford Bernard <clifford@verticalfront.com>
2020-03-28 21:39:00 -0500

    MongoDB and Mongoose Commit:
    Added MongoDB and Mongoose to the project.  Updated the places-controllers controller file to interact with the Mongo database.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -13,6 +14,15 @@ app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 app.use(defaultRoutes);
 
-app.listen(5000, () => {
-  console.log("App started; server is listening on port 5000");
-});
+mongoose.connect("mongodb+srv://admin:BEZoNEvzfnhyJxYq@cluster0-6jl11.gcp.mongodb.net/places?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("App started; server is listening on port 5000");
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,99 +1,117 @@
-const {v4: uuid} = require("uuid");
 const {validationResult} = require("express-validator");
 
+const Place = require("../models/Place");
 const HttpResponse = require("../models/http-response");
 const getLocationFromAddress = require("../util/location");
 
-let PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrappers in the world.",
-    imageURL: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/33/e6.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {lat: 40.7484405, lng: -73.9856644},
-    creator: "u1"
-  },
-  {
-    id: "p2",
-    title: "Chrysler Building",
-    description: "The Chrysler Building is an Art Deco–style skyscraper located in the Turtle Bay neighborhood on the East Side of Manhattan, New York City, at the intersection of 42nd Street and Lexington Avenue near Midtown Manhattan.",
-    imageURL: "https://static.dezeen.com/uploads/2019/01/chrysler-building-for-sale-news-architecture-new-york-city-dezeen-2364-shutterstock-65007532-sq.jpg",
-    address: "405 Lexington Ave, New York, NY 10174",
-    location: {lat: 40.7516208, lng: -73.975502},
-    creator: "u2"
-  },
-  {
-    id: "p3",
-    title: "Niagara Falls State Park",
-    description: "Niagara Falls is a group of three waterfalls at the southern end of Niagara Gorge, spanning the border between the US state of New York and the Canadian province of Ontario.",
-    imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/3Falls_Niagara.jpg/1280px-3Falls_Niagara.jpg",
-    address: "24 Buffalo Avenue\n Niagara Falls, NY, 14303",
-    location: {lat: 43.081528, lng: -79.064240},
-    creator: "u2"
-  }
-];
-
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res) => {
   const httpResponse = new HttpResponse(res);
-  const place = PLACES.find(p => p.id === req.params.placeId);
+
+  let place;
+  try {
+    place = await Place.findById(req.params.placeId);
+  } catch (err) {
+    return httpResponse.error(500, "Fetching place by Id failed, please try again later", 500);
+  }
 
   if (!place) return httpResponse.error(404, "Could not find a place with the provided place id.");
-  return httpResponse.send({place: place});
+  return httpResponse.send({place: place.toObject({getters: true})});
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res) => {
   const httpResponse = new HttpResponse(res);
-  const places = PLACES.filter(p => p.creator === req.params.userId);
 
-  if (places.length === 0) return httpResponse.error(404, "Could not find places with the provided user id.");
-  return httpResponse.send({places: places});
+  let places;
+  try {
+    places = await Place.find({creator: req.params.userId});
+  } catch (err) {
+    return httpResponse.error(500, "Fetching places failed, please try again later", 500);
+  }
+
+  if (!places || places.length === 0) return httpResponse.error(404, "Could not find places with the provided user id.");
+  return httpResponse.send({places: places.map(p => p.toObject({getters: true}))});
 };
 
-const createPlace = async (req, res, next) => {
+const createPlace = async (req, res) => {
   const httpResponse = new HttpResponse(res);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) return httpResponse.error(422, errors.errors, 401);
 
-  const {title, description, address, creator} = req.body;
-  const location = await getLocationFromAddress(address);
+  const {title, description, address, creator, imageURL} = req.body;
 
+  const location = await getLocationFromAddress(address);
   if (location.errors) return httpResponse.error(422, location.errors, 401);
 
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title: title,
     description: description,
     location: location,
     address: address,
     creator: creator,
-    imageURL: ""
-  };
-  PLACES.push(createdPlace);
+    imageURL: imageURL
+  });
 
-  return httpResponse.send({place: createdPlace});
+  let result;
+  try {
+    result = await createdPlace.save();
+  } catch (err) {
+    return httpResponse.error(500, "Could not create place, please try again later", 500);
+  }
+
+  return httpResponse.send({place: result.toObject({getters: true})});
 };
 
-const updatePlaceById = (req, res, next) => {
+const updatePlaceById = async (req, res) => {
   const httpResponse = new HttpResponse(res);
   const updatePlace = req.body;
   const errors = validationResult(req);
-  const placeIndex = PLACES.findIndex(p => p.id === req.params.placeId);
 
   if (!errors.isEmpty()) return httpResponse.error(422, errors.errors, 401);
-  else if (placeIndex < 0) return httpResponse.error(404, "Could not find place with the provided id.");
-  else PLACES[placeIndex] = updatePlace;
 
-  return httpResponse.send({place: updatePlace});
+  let place;
+  try {
+    place = await Place.findById(req.params.placeId);
+  } catch (err) {
+    return httpResponse.error(500, "Fetching place by Id failed, please try again later", 500);
+  }
+
+  if (updatePlace.title && place.title !== updatePlace.title) place.title = updatePlace.title;
+  if (updatePlace.description && place.description !== updatePlace.description) place.description = updatePlace.description;
+  if (updatePlace.imageURL && place.imageURL !== updatePlace.imageURL) place.imageURL = updatePlace.imageURL;
+  if (updatePlace.address && place.address !== updatePlace.address) {
+    const location = await getLocationFromAddress(updatePlace.address);
+
+    if (location.errors) return httpResponse.error(422, location.errors, 401);
+
+    place.address = updatePlace.address;
+    place.location = location;
+  }
+
+  try {
+    await place.save()
+  } catch (err) {
+    return httpResponse.error(500, "Could not update place, please try again later", 500);
+  }
+
+  return httpResponse.send({place: place.toObject({getters: true})});
 };
 
-const deletePlaceById = (req, res, next) => {
+const deletePlaceById = async (req, res) => {
   const httpResponse = new HttpResponse(res);
-  const place = PLACES.find(p => p.id === req.params.placeId);
 
-  if (!place) return httpResponse.error(404, "Could not delete place with the provided id.");
-  else PLACES = PLACES.filter(p => p.id !== req.params.placeId);
+  let place;
+  try {
+    place = await Place.findById(req.params.placeId);
+  } catch (err) {
+    return httpResponse.error(500, "Fetching place by Id failed, please try again later", 500);
+  }
+
+  try {
+    await place.remove();
+  } catch (err) {
+    return httpResponse.error(500, "Could not delete place, please try again later", 500);
+  }
 
   return httpResponse.send({});
 };
